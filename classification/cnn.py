241b27dcd6ecddae03518521b01c24b8b819b71a
Manthan Shah <manthan711@gmail.com>
2021-01-15 13:46:24 -0500

    Fix data input to include freq band dimension, and fix CNN architecture based on paper

--- classification/cnn.py
+++ classification/cnn.py
@@ -3,88 +3,108 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 import tensorflow.keras as keras
 
+# Replace this line with location of JSON data file
 DATA_PATH = "/Users/manthanshah/Desktop/data.json"
 
 def load_data(data_path):
+    """
+    Loads the data to be fed into the CNN.
+
+    Args:
+        data_path (string): the path at which the JSON data file is stored
+    Returns:
+        X (ndarray): the data values
+        y (ndarray): the labels for the data
+    """
+
     with open(data_path, "r") as fp:
         data = json.load(fp)
     
-    X = np.array(data["mfcc"])
+    X = np.array(data["values"])
     y = np.array(data["labels"])
-    
+
     return X, y
 
-def prepare_datasets(test_size, validation_size):
-    """Loads data and splits it into train, validation and test sets.
-    :param test_size (float): Value in [0, 1] indicating percentage of data set to allocate to test split
-    :param validation_size (float): Value in [0, 1] indicating percentage of train set to allocate to validation split
-    :return X_train (ndarray): Input training set
-    :return X_validation (ndarray): Input validation set
-    :return X_test (ndarray): Input test set
-    :return y_train (ndarray): Target training set
-    :return y_validation (ndarray): Target validation set
-    :return y_test (ndarray): Target test set
+def prepare_datasets(test_size, validation_size, should_add_axis=False):
+    """
+    Loads data and splits it into train, validation and test sets.
+
+    Args:
+        test_size (float): Value in [0, 1] indicating percentage of data set to allocate to test split
+        validation_size (float): Value in [0, 1] indicating percentage of train set to allocate to validation split
+        should_add_axis (bool): If the data has depth one, set to True
+    Returns:
+        X_train (ndarray): Input training set
+        X_validation (ndarray): Input validation set
+        X_test (ndarray): Input test set
+        y_train (ndarray): Target training set
+        y_validation (ndarray): Target validation set
+        y_test (ndarray): Target test set
     """
 
-    # load data
+    # Load data
     X, y = load_data(DATA_PATH)
 
-    # create train, validation and test split
+    # Create train, validation and test split
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
     X_train, X_validation, y_train, y_validation = train_test_split(X_train, y_train, test_size=validation_size)
 
-    # add an axis to input sets
-    X_train = X_train[..., np.newaxis] # 4-D array
-    X_validation = X_validation[..., np.newaxis]
-    X_test = X_test[..., np.newaxis]
+    if should_add_axis:
+        # add an axis to input sets
+        X_train = X_train[..., np.newaxis] # 4-D array
+        X_validation = X_validation[..., np.newaxis]
+        X_test = X_test[..., np.newaxis]
 
     return X_train, X_validation, X_test, y_train, y_validation, y_test
 
 def build_model(input_shape):
-    """Generates CNN model
-    :param input_shape (tuple): Shape of input set
-    :return model: CNN model
+    """
+    Generates CNN model
+
+    Args:
+        input_shape (tuple): Shape of input set
+    Returns:
+        model: CNN model
     """
 
-    # build network topology
+    # Build network topology
     model = keras.Sequential()
 
     # 1st conv layer
-    model.add(keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
-    model.add(keras.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same'))
-    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Conv2D(8, (5, 5), activation='relu', input_shape=input_shape))
+    model.add(keras.layers.MaxPooling2D((2, 2), padding='same'))
 
     # 2nd conv layer
-    model.add(keras.layers.Conv2D(32, (3, 3), activation='relu'))
-    model.add(keras.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same'))
-    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Conv2D(4, (5, 5), activation='relu'))
+    model.add(keras.layers.MaxPooling2D((2, 2), padding='same'))
+    model.add(keras.layers.Dropout(0.25))
 
-    # flatten output and feed it into dense layer
+    # Flatten output and feed it into dense layer
     model.add(keras.layers.Flatten())
-    model.add(keras.layers.Dense(64, activation='relu'))
-    model.add(keras.layers.Dropout(0.3))
+    model.add(keras.layers.Dense(20, activation='relu'))
+    model.add(keras.layers.Dropout(0.5))
 
-    # output layer
-    model.add(keras.layers.Dense(10, activation='softmax'))
+    # Output layer
+    model.add(keras.layers.Dense(2, activation='sigmoid'))
 
     return model
 
-# get train, validation, test splits
+# Get train, validation, test splits
 X_train, X_validation, X_test, y_train, y_validation, y_test = prepare_datasets(0.25, 0.2)
 
-# create network
-input_shape = (X_train.shape[1], X_train.shape[2], 1)
+# Create network
+input_shape = (X_train.shape[1], X_train.shape[2], X_train.shape[3])
 model = build_model(input_shape)
 
-# compile model
-optimiser = keras.optimizers.Adam(learning_rate=0.0001)
-model.compile(optimizer=optimiser, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+# Compile model
+optimizer = keras.optimizers.Adam(learning_rate=0.0001)
+model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 model.summary()
 
-# train the CNN
-model.fit(X_train, y_train, validation_data=(X_validation, y_validation), batch_size=32, epochs=30)
+# Train the CNN
+model.fit(X_train, y_train, validation_data=(X_validation, y_validation), batch_size=32, epochs=100)
 
-# evaluate the CNN on the test set
+# Evaluate the CNN on the test set
 test_error, test_accuracy = model.evaluate(X_test, y_test, verbose=2)
 print("Accuracy on the test set is: {}".format(test_accuracy))
--- classification/cnn_preprocess.py
+++ classification/cnn_preprocess.py
@@ -3,9 +3,25 @@ import os
 import math
 import librosa
 import scipy.signal as signal
-from segmentation_util import custom_loadmat
+from segmentation_util import custom_loadmat, get_transitions
+import numpy as np
 
-def frequency_decomposition(pcg, N=60, sr=1000):
+def frequency_decomposition(pcg, N=60, sr=2000):
+    """
+    Decomposes a PCG signal into 4 distinct frequency bands of:
+        1. 0 to 45 Hz
+        2. 45 - 80 Hz
+        3. 80 - 200 Hz
+        4. 200 - 400 Hz
+    
+    Args:
+        pcg (array): input signal to be decomposed
+        N (int): length of the filter (filter order + 1)
+        sr (int): sampling rate of the input signal
+    Returns:
+        pcg_decomposed (ndarray): an array of arrays corresponding to each frequency band
+    """
+
     Wn = (45 * 2) / sr
     b1 = signal.firwin(N + 1, Wn, window='hamming', pass_zero='lowpass')
 
@@ -18,13 +34,11 @@ def frequency_decomposition(pcg, N=60, sr=1000):
     Wn = (200 * 2) / sr
     b4 = signal.firwin(N + 1, Wn, window='hamming', pass_zero='highpass')
 
-    return b1, b2, b3, b4
-
     return [
-        scipy.signal.filtfilt(b1, 1, pcg),
-        scipy.signal.filtfilt(b2, 1, pcg),
-        scipy.signal.filtfilt(b3, 1, pcg),
-        scipy.signal.filtfilt(b4, 1, pcg)
+        signal.filtfilt(b1, 1, pcg),
+        signal.filtfilt(b2, 1, pcg),
+        signal.filtfilt(b3, 1, pcg),
+        signal.filtfilt(b4, 1, pcg)
     ]
 
 def remove_prefix(text, prefix):
@@ -33,11 +47,18 @@ def remove_prefix(text, prefix):
     return text
 
 def get_data(json_path="/Users/manthanshah/Desktop/data.json"):
-    # Dictionary to store mapping, labels, and MFCCs
+    """
+    Converts audio data into JSON data file for input to CNN.
+
+    Args:
+        json_path: the location where to store the output data
+    """
+
+    # Dictionary to store mapping, labels, and values
     data = {
         "mapping": ["normal", "abnormal"],
         "labels": [],
-        "mfcc": []
+        "values": []
     }
 
     # Get labels
@@ -51,22 +72,41 @@ def get_data(json_path="/Users/manthanshah/Desktop/data.json"):
     for filename in filenames:
         pcg, sr = librosa.load(f"{filename}.wav", sr=2000)
 
-        # todo
-        # b1, b2, b3, b4 = frequency_decomposition(pcg, sr=sr)
+        pcg_decomposed = frequency_decomposition(pcg, sr=sr)
+        pcg_states = custom_loadmat(f"{filename}_states.mat")['assigned_states']
 
-        mfcc = librosa.feature.mfcc(y=pcg, sr=sr, n_mfcc=13, n_fft=2048, hop_length=512)
-        mfcc = mfcc.T
-        
-        if len(mfcc) == 32:
-            data["mfcc"].append(mfcc.tolist())
+        transitions = get_transitions(pcg_states)
 
-            if remove_prefix(filename, "./challenge_data/") in abnormal_records:
-                data["labels"].append(1)
-            else:
-                data["labels"].append(0)
+        num_freq_bands = 4
+        num_samples = 2500 # 2.5 s
+        num_cardiac_cycles = min(32, len(transitions) - 1) # cap at 32 cycles per signal
+
+        if num_cardiac_cycles < 32:
+            continue
+
+        # Data shape: 32 cycles x 2500 samples / cycle x 4 bands
+        X = np.zeros(shape=(num_cardiac_cycles, num_samples, num_freq_bands))
+
+        for cardiac_cycle in range(num_cardiac_cycles):
+            for freq_band in range(num_freq_bands):
+                values = pcg_decomposed[freq_band][transitions[cardiac_cycle]:transitions[cardiac_cycle + 1]]
+
+                if len(values) > 2500:
+                    values = values[:len(values)-2500]
+
+                for sample in range(len(X[cardiac_cycle])):
+                    if sample < len(values):
+                        X[cardiac_cycle][sample][freq_band] = values[sample]
+
+        data["values"].append(X.tolist())
+        if remove_prefix(filename, "./challenge_data/") in abnormal_records:
+            data["labels"].append(1)
+        else:
+            data["labels"].append(0)
     
-    # save MFCCs to json file
+    # save values to json file
     with open(json_path, "w") as fp:
         json.dump(data, fp, indent=4)
 
+# Run this to pre-process any data for input to the CNN
 get_data()
